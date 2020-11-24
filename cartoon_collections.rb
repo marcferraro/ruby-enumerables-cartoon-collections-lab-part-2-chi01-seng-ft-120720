5aa924d136ca893a073da35721dc32c56a9b2d8a
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-24 21:54:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,8 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.map find |element| do
-    element 
+    element.length > 4
+  end
   
 end
 
